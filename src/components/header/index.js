2a03dc2b8013236e9c10302ca399b73448b72b2d
Grace_ji <80709412@qq.com>
2019-10-31 10:55:09 +0800

    init

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import { View, StatusBar, TouchableOpacity } from 'react-native';
+
+export default class RNHeader extends PureComponent {
+  render () {
+    return (
+      <View>
+        
+      </View>
+    );
+  }
+}
---
+++ src/components/header/styles.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+});
+
+export default StyleSheet.flatten(styles);
