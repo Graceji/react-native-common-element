9130a7a245f743939d4f56bd969daa332d7ef084
Grace_ji <80709412@qq.com>
2019-11-18 17:28:32 +0800

    dropdown没有导出

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ export { isIphoneX, screenUtil, screenW, screenH } from './src/utils'; // 屏幕
 export { default as RNHeader } from './src/components/header'; // 头部组件
 export { default as RNLoading } from './src/components/loading'; // loading
 export { default as RNEchart } from './src/components/echart'; // Echart
+export { default as RNCollapse } from './src/components/collapse'; // collapse
+export { default as RNDropDown } from './src/components/dropDown'; // dropdown
